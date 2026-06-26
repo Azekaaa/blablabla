bb6b306d9bf62247a7278dba50bc9223ce259c84
Ақзере Мысырова <akzere.myssyrova@sanasport.kz>
2026-06-26 11:06:09 +0500

    Fix: use get_closed_deals_month

--- bot/services/sync_service.py
+++ bot/services/sync_service.py
@@ -35,8 +35,9 @@ class SyncService:
 
             # Fetch all active deals
             raw_deals = await self.client.get_all_deals()
+            
             # Также фетчим закрытые сделки для конверсии
-            raw_closed = await self.client.get_closed_deals_today()
+            raw_closed = await self.client.get_closed_deals_month()
             raw_deals_all = raw_deals + raw_closed
 
             # Collect unique responsible IDs
